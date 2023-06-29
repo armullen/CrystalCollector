3000db361ea5de93d8cf78ab9c0a24269e6ea381
armullen <amber.r.mullen@gmail.com>
2023-06-29 00:21:22 -0500

    working on routes

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
-urlpatterns - [
-    
+urlpatterns = [
+   path('', views.Home.as_view(), name='home'), 
+   path('about/', views.About.as_view(), name="about")
 ]
