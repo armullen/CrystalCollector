0005d8dd6a95268b82f22fef9126591536e0233f
armullen <amber.r.mullen@gmail.com>
2023-06-30 21:22:42 -0500

    moving on to models

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
    path('', views.Home.as_view(), name='home'), 
-   path('about/', views.About.as_view(), name="about")
+   path('about/', views.About.as_view(), name="about"),
+   path('crystals/', views.CrystalList.as_view(), name='crystal_list')
 ]
